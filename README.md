cd4704f121a3323a211b0f702116021782adeb27
wangxian <xian366@126.com>
2016-03-14 00:47:09 +0800

    修改：添加redis password支持

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Application will start `./startup.sh` Automatically.
 docker build -t wangxian/alpine-redis .
 docker run -it --rm -p 6379:6379 wangxian/alpine-redis
 ```
+
+# Set redis password
+
+docker run -it -e REDIS_PASSWORD=1122334455 --rm -p 6379:6379 wangxian/alpine-redis
--- startup.sh
+++ startup.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-redis-server /app/redis.conf
+
+echo "[i] Update redis config file"
+cp -vf ./redis.conf /etc/redis.conf
+
+REDIS_PASSWORD=${REDIS_PASSWORD:-""}
+if [ "$REDIS_PASSWORD" != "" ]; then
+  echo "[i] Update redis masterauth=$REDIS_PASSWORD"
+  echo "requirepass $REDIS_PASSWORD" >> /etc/redis.conf
+fi
+
+redis-server /etc/redis.conf
