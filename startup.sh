a22dda2500882fce62ccac9e93525996e9893603
wangxian <xian366@126.com>
2016-03-25 10:58:50 +0800

    修改：配置文件

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ cp -vf ./redis.conf /etc/redis.conf
 
 REDIS_PASSWORD=${REDIS_PASSWORD:-""}
 if [ "$REDIS_PASSWORD" != "" ]; then
-  echo "[i] Update redis masterauth=$REDIS_PASSWORD"
+  echo "[i] Update redis requirepass=$REDIS_PASSWORD"
   echo "requirepass $REDIS_PASSWORD" >> /etc/redis.conf
 fi
 
