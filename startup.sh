cdeff4c7942d7a6cc89ba6f29e4c6c6fdaf0d810
wangxian <xian366@126.com>
2024-04-18 01:09:46 +0800

    update: installed exists, not add requirepass

--- startup.sh
+++ startup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 
-REDIS_PASSWORD=${REDIS_PASSWORD:-""}
+if [ ! -f /installed ]; then
+  REDIS_PASSWORD=${REDIS_PASSWORD:-""}
+  
+  touch /installed
 
-if [ "$REDIS_PASSWORD" != "" ]; then
-  echo "[i] Update redis requirepass=$REDIS_PASSWORD"
-  echo "requirepass $REDIS_PASSWORD" >> /etc/redis.conf
+  if [ "$REDIS_PASSWORD" != "" ]; then
+    echo "[i] Update redis requirepass=$REDIS_PASSWORD"
+    echo "requirepass $REDIS_PASSWORD" >> /etc/redis.conf
+  fi
+
 fi
 
 redis-server /etc/redis.conf
